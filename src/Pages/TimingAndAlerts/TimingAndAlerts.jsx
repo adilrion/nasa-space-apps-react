a02f8a37d8cc22e169df602d44e879dae51abb56
AdilMahmoudRion <adilmahmoudrion@gmail.com>
2023-10-06 04:54:53 +0600

    react timeline

--- src/Pages/TimingAndAlerts/TimingAndAlerts.jsx
+++ src/Pages/TimingAndAlerts/TimingAndAlerts.jsx
@@ -1,60 +1,73 @@
 import React from "react";
-import { MapContainer, Polygon, ScaleControl, TileLayer } from "react-leaflet";
+import { MapContainer, Polygon, TileLayer } from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
 import { statesData } from "./data";
 
-var center = [39.85469173654045, -101.29976334521231];
+
 
 export const TimingAndAlerts = () => {
 
+  const center = [40.63463151377654, -97.89969605983609]; // Set to a specific location
 
   return (
     <div className="max-w-screen-xl mx-auto">
-
       <header className="pt-10 py-6">
         <h1 className="underline decoration-wavy underline-offset-4">Timing And Alerts</h1>
       </header>
-
-      <section className="z-50 relative">
-
+      <section className="">
         <MapContainer
-          center={[39.85469173654045, -101.29976334521231]}
-          zoom={13}
-          style={{ width: '100%', height: '100%', zIndex: 100 }}
+          center={center}
+          zoom={10}
+          style={{ width: '100%', height: '500px'}}
         >
-
           <TileLayer
-            
+            url={`https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=AQ2fhr4c49NuzXXS86KJ`}
             attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
-            url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=xB4gUAy4lpRWHG1IW2t4"
           />
-
-          {statesData?.features?.map((data, index) => {
-            // Ensure that the transformation matches the coordinate format of your data
+          {statesData.features?.map((data, index) => {
             const coordinates = data.geometry.coordinates[0]?.map((item) => [item[1], item[0]]) || [];
-
             return (
               <Polygon
-                key={index}
                 pathOptions={{
-                  fillColor: '#FD8D36',
+                  fillColor: '#FD8D3C',
                   fillOpacity: 0.7,
                   weight: 2,
                   opacity: 1,
                   dashArray: 3,
-                  color: 'white',
+                  color: 'white'
                 }}
                 positions={coordinates}
+                eventHandlers={{
+                  mouseover: (e) => {
+                    const layer = e.target;
+                    layer.setStyle({
+                      dashArray: "",
+                      fillColor: "#BD0026",
+                      fillOpacity: 0.7,
+                      weight: 2,
+                      opacity: 1,
+                      color: "white",
+                    })
+                  },
+                  mouseout: (e) => {
+                    const layer = e.target;
+                    layer.setStyle({
+                      fillOpacity: 0.7,
+                      weight: 2,
+                      dashArray: "3",
+                      color: 'white',
+                      fillColor: '#FD8D3C'
+                    });
+                  },
+                  click: (e) => {
+
+                  }
+                }}
               />
             );
           })}
-
-          
-          <ScaleControl />
         </MapContainer>
-
       </section>
-
     </div>
   );
 };
